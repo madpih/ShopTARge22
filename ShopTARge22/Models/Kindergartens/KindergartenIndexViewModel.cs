a65235a0291bac50bba0636d05385c890cd015f8
madis <madis.pihlasalu@gmail.com>
2023-10-12 13:54:39 +0300

    create viewmodels

---
+++ ShopTARge22/Models/Kindergartens/KindergartenIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopTARge22.Models.Kindergartens
+{
+    public class KindergartenIndexViewModel
+    {
+        public Guid? Id { get; set; }
+        public string GroupName { get; set; }
+        public int ChildrenCount { get; set; }
+        public string KindergartenName { get; set; }
+        public string Teacher { get; set; }
+
+    }
+}
